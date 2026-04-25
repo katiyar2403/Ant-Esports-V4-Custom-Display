2b056a18f3fb86e0cd0340f9e91c30eee5eb7758
shreyansh <shreyansh@talkndheal.com>
2026-04-25 17:16:30 +0530

    Update AssemblyInfo with proper metadata

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,33 +1,27 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AntEsportsV4Silent")]
-[assembly: AssemblyDescription("")]
+
+[assembly: AssemblyTitle("Ant Esports V4 Custom Display")]
+[assembly: AssemblyDescription("Custom tray application for Ant Esports V4 Digital CPU Air Cooler")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AntEsportsV4Silent")]
-[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyCompany("Personal Project")]
+[assembly: AssemblyProduct("Ant Esports V4 Custom Display")]
+[assembly: AssemblyCopyright("Copyright © 2026 katiyar2403")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
+// to COM components. If you need to access a type in this assembly from COM,
+// set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bb6d6d7b-9d32-49a4-8dce-20b80a858ab8")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
+// Version information for an assembly
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
